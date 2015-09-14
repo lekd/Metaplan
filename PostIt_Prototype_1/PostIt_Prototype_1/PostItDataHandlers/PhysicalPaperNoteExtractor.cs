a8d3326becb6041e01136f99071118bc4da5a7b3
lekd <duylekhanh@gmail.com>
2015-09-14 18:35:15 +0200

    Add source code of the Metaplan prototype

---
+++ PostIt_Prototype_1/PostIt_Prototype_1/PostItDataHandlers/PhysicalPaperNoteExtractor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PostIt_Prototype_1.PostItDataHandlers
+{
+    public class PhysicalPaperNoteExtractor:IPostItDataHandler
+    {
+        static byte[] standardPrefix = new byte[] { (byte)'<', (byte)'T', (byte)'A', (byte)'B', (byte)'L', (byte)'E', (byte)'-'};
+        static byte[] standardPostfix = new byte[] { (byte)'<', (byte)'/', (byte)'T', (byte)'A', (byte)'B', (byte)'L', (byte)'E', (byte)'-' };
+    }
+}
---
+++ PostIt_Prototype_1/PostIt_Prototype_1/Utilities/UtilitiesLib.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace PostIt_Prototype_1.Utilities
+{
+    public class UtilitiesLib
+    {
+
+        public static double distanceBetweenTwoPoints(float x1, float y1, float x2, float y2)
+        {
+            return Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+        }
+        public static PointF convertRelativeCoordinateToGlobalCoordinate(float relativeX, float relativeY, float screenW, float screenH)
+        {
+            PointF globalCoordinate = new PointF();
+            globalCoordinate.X = relativeX * screenW;
+            globalCoordinate.Y = relativeY * screenH;
+            return globalCoordinate;
+        }
+        public static int Bytes2Int(byte[] bytes)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            return BitConverter.ToInt32(bytes,0);
+        }
+        public static float Bytes2Float(byte[] bytes)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            return BitConverter.ToSingle(bytes, 0);
+        }
+        public static BitmapImage convertBitmapToBitmapImage(Bitmap bmp)
+        {
+            MemoryStream ms = new MemoryStream();
+            bmp.Save(ms,System.Drawing.Imaging.ImageFormat.Png);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+    }
+}
