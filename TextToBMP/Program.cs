a237b05c4c9537e860ed189aa018d8d4f97dc914
Ali Alavi <alavis@ethz.ch>
2015-11-20 15:41:26 +0100

    TextToBMP added

---
+++ TextToBMP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace TextToBMP
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var test = new StreamReader ("test5.txt");
+			var str = test.ReadToEnd ();
+			Console.WriteLine ("{0}", str.Length);
+
+			var text2png = new Text2PNG (5.0f);
+			var output = text2png.FromString (str, 2000, 2000);
+			output.Save ("test3.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ TextToBMP/Text2BMP.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace TextToBMP
+{
+	public class Text2BMP
+	{		
+		float Scale = 1.0f;
+		public Text2BMP (float scale = 1.0f)
+		{	
+			Scale = scale;		
+		}
+
+		public Bitmap FromString(String fullStr, int width, int height)
+		{			
+			var lines = fullStr.Split ('\n');
+			Console.WriteLine ("Number of lines= {0}", lines.Length);
+			var bmp = new Bitmap (width, height);
+			var drawing = Graphics.FromImage (bmp);
+			drawing.FillRectangle (Brushes.White, 0, 0, width, height);
+
+			foreach (var str in lines) {
+				if (str == "")
+					continue;
+				var strPoints = str.Split (' ');
+				var numberOfPoints = (strPoints.Length - 1) / 2;
+				Console.WriteLine (strPoints);
+				Console.WriteLine (numberOfPoints);
+				// Last substr is always null string
+				var points = new PointF[numberOfPoints];
+
+				for (int i = 0; i < numberOfPoints; i++)
+					points [i] = new PointF (float.Parse (strPoints [2 * i]) / this.Scale, float.Parse (strPoints [2 * i + 1]) / this.Scale);
+
+				drawing.DrawLines(Pens.Black, points);
+			}
+			return bmp;
+		}
+	}
+}
+
